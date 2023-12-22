516cf2c3151eed9825b51a9acbedd8765d6e7b1c
anik-biswas <anik.biswas.an@gmail.com>
2023-12-22 11:35:21 +0600

    added a visitor section in homepage manually

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../banner/Banner';
+import Visitor from '../visitor/Visitor';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Visitor></Visitor>
         </div>
     );
 };
